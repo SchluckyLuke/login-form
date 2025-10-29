08f9a50273d4409f1f82cece13dee2590623ebcb
SchluckyLuke <matthaeus.h24@gmail.com>
2025-10-30 00:10:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const usernameInput = document.getElementById("usernameInput");
+const passwordInput = document.getElementById("passwordInput");
+const myH3 = document.getElementById("myH3");
+
+function viewPassword(){
+    myH3.textContent = `Your username is ${usernameInput.value} and your password is ${passwordInput.value}`;
+}
